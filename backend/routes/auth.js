ca305931b1f272a61d8f4c35e5d4b15c375a2a87
Arpana Sitoula <arp2ctaula@gmail.com>
2023-01-03 21:46:18 +0545

    removed obj

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res)=>{
-    obj = {
-        name: "Arps",
-        number: 2
-    }
-    res.json(obj)
+    res.json([])
 })
 module.exports = router;
